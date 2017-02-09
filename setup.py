1511b90a475741ba1c8e30ba037e8c18789ac6e5
D. MacCarthy <devwigs@gmail.com>
2017-02-08 18:55:53 -0700

    setup.py
    Fix blanks

--- setup.py
+++ setup.py
@@ -5,21 +5,21 @@ archive = "master" if __dev__ else "v{}".format(ver)
 with open("README.txt") as f: readme = f.read()
 
 setup(
-	# Package info
+    # Package info
     name = "sc8pr",
     version = ver,
     license = "GPLv3",
     packages = ["sc8pr"],
-	package_data = {"sc8pr": ["*.json", "icons/*.*", "robot/*.*"]},
+    package_data = {"sc8pr": ["*.json", "icons/*.*", "robot/*.*"]},
 
     # Author
     author = "David MacCarthy",
     author_email = "devwigs@gmail.com",
 
-	# Dependencies
-	install_requires = ["pygame(>=1.9.1)"],
-	
-	# URLs
+    # Dependencies
+    install_requires = ["pygame(>=1.9.1)"],
+    
+    # URLs
     url = "http://dmaccarthy.github.io/sc8pr",
     download_url = "https://github.com/dmaccarthy/sc8pr/archive/{}.zip".format(archive),
 
@@ -27,15 +27,15 @@ setup(
     description = "Create interactive animations with features inspired by Scratch, Processing, and robotics",
     long_description = readme,
 
-	# Additional data
+    # Additional data
     keywords = "graphics animation sprite gui robotics pygame educational",
     classifiers = [
         "Development Status :: 4 - Beta",
-		"Intended Audience :: Developers",
-		"Intended Audience :: Education",
-		"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-		"Programming Language :: Python :: 3",
-		"Topic :: Multimedia :: Graphics",
-		"Topic :: Education"
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Topic :: Multimedia :: Graphics",
+        "Topic :: Education"
     ]
 )
