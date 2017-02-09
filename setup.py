4eff9cd8c36a9b586882d35dc9bb18f0e55d449e
D. MacCarthy <devwigs@gmail.com>
2017-02-08 18:42:48 -0700

    setup.py
    Allow master as download_url

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
-from sc8pr import __version__ as ver
+from sc8pr import __version__ as ver, __dev__
 
+archive = "master" if __dev__ else "v{}".format(ver)
 with open("README.txt") as f: readme = f.read()
 
 setup(
@@ -8,19 +9,19 @@ setup(
     name = "sc8pr",
     version = ver,
     license = "GPLv3",
-    packages=["sc8pr"],
-	package_data={"sc8pr": ["*.json", "icons/*.*", "robot/*.*"]},
+    packages = ["sc8pr"],
+	package_data = {"sc8pr": ["*.json", "icons/*.*", "robot/*.*"]},
 
     # Author
     author = "David MacCarthy",
     author_email = "devwigs@gmail.com",
 
 	# Dependencies
-	install_requires=['pygame(>=1.9.1)'],
+	install_requires = ["pygame(>=1.9.1)"],
 	
 	# URLs
     url = "http://dmaccarthy.github.io/sc8pr",
-    download_url = "https://github.com/dmaccarthy/sc8pr/archive/v{}.zip".format(ver),
+    download_url = "https://github.com/dmaccarthy/sc8pr/archive/{}.zip".format(archive),
 
     # Details
     description = "Create interactive animations with features inspired by Scratch, Processing, and robotics",
