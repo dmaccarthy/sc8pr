219609bc92a9bae60a80e03e54299e4eb4d80936
D. MacCarthy <devwigs@gmail.com>
2017-10-17 17:00:54 -0600

    GitHub Desktop Glitch

--- sc8pr/sprite.py
+++ sc8pr/sprite.py
@@ -14,7 +14,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with "sc8pr".  If not, see <http://www.gnu.org/licenses/>.
-
+#
 
 from math import hypot
 from pygame.sprite import collide_mask, collide_rect, collide_circle
