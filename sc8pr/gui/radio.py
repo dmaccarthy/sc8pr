72a473d4f6ecec8414dd76ad616a72301a7603fe
D. MacCarthy <sc8pr.py@gmail.com>
2018-04-22 15:09:16 -0600

    radio Module
    Change constructor parameters

--- sc8pr/gui/radio.py
+++ sc8pr/gui/radio.py
@@ -25,22 +25,22 @@ class Options(Canvas):
     """GUI control consisting of check boxes and text;
     buttons handle onclick; trigger onaction"""
 
-    def __init__(self, text, height=None, space=4, imgs=None, **kwargs):
-        text = [Text(t).config(**kwargs) for t in text]
+    def __init__(self, text, height=None, padding=4, imgs=None, txtConfig={}):
+        text = [Text(t).config(**txtConfig) for t in text]
         check = []
         w = 0
-        y = space
+        y = padding
         if not height:
             height = 1 + text[0].height // len(text[0].data.split("\n"))
         for t in text:
             cb = Button.checkbox(imgs).config(height=height)
             yc = y + t.height / 2
-            check.append(cb.config(height=height, pos=(space, yc), anchor=LEFT))
-            t.config(pos=(cb.width + 2 * space, yc), anchor=LEFT, **kwargs)
-            y += t.height + space
+            check.append(cb.config(height=height, pos=(padding, yc), anchor=LEFT))
+            t.config(pos=(cb.width + 2 * padding, yc), anchor=LEFT, **txtConfig)
+            y += t.height + padding
             w1 = cb.width + t.width
             if w1 > w: w = w1
-        super().__init__((w + 3 * space, y))
+        super().__init__((w + 3 * padding, y))
         self += check + text
         self.boxes = check
         if hasattr(self, "selected"): self.selected = 0
@@ -50,9 +50,9 @@ class Radio(Options):
     """GUI control consisting of radio buttons and text;
     buttons handle onclick; radio handles onaction and triggers onchange"""
 
-    def __init__(self, text, height=None, space=4, imgs=None, **kwargs):
+    def __init__(self, text, height=None, padding=4, imgs=None, txtConfig={}):
         if imgs is None: imgs = Button._radioTiles()
-        super().__init__(text, height, space, imgs, **kwargs)
+        super().__init__(text, height, padding, imgs, txtConfig)
 
     @property
     def selected(self):
