9fdabaf1e712f8e3cef3f0be076ec9cdf9423b2c
D. MacCarthy <sc8pr.py@gmail.com>
2019-05-21 11:21:19 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     # Additional data
     keywords = "graphics animation sprite gui robotics pygame educational",
     classifiers = [
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "Intended Audience :: Education",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
