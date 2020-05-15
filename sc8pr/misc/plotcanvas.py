58da4223081074b3e9d4b553ec319831fe71351a
D. MacCarthy <sc8pr.py@gmail.com>
2020-05-15 15:24:37 -0600

    Update plotcanvas.py

--- sc8pr/misc/plotcanvas.py
+++ sc8pr/misc/plotcanvas.py
@@ -18,32 +18,81 @@
 
 from math import cos, sin, sqrt
 import pygame
-from sc8pr import Renderable, Graphic, Canvas, Image, CENTER
+from sc8pr import Sketch, Renderable, Graphic, Canvas, Image, CENTER
 from sc8pr.text import Text
 from sc8pr.geom import sigma, polar2d, transform2d, smallAngle, DEG
-from sc8pr.shape import Arrow, Line
+from sc8pr.shape import Arrow, Line, Polygon
 from sc8pr.misc import plot as oldPlot
 from sc8pr.misc.plot import coordTr, _lrbt, locus
+from sc8pr.util import mix
 
 
 calc_lrbt = _lrbt
 
 
-class PCanvas(Canvas):
+class _Plotable:
+    _plot = True
 
-    def __init__(self, size, lrbt, bg=None):
-        super().__init__(size, bg)
-        self._makeTr(lrbt)
-    
-    def _makeTr(self, lrbt=None):
-        if lrbt: self._lrbt = lrbt = calc_lrbt(lrbt, *self.size)
-        else: lrbt = self._lrbt
-        self.pix = px = coordTr(lrbt, self.size)
-        self.unpix = coordTr(lrbt, self.size, True)
+    @property
+    def plot(self):
+        p = self._plot
+        if p is True:
+            p = getattr(self, "canvas", None)
+            if not isinstance(p, CoordinateSystem): p = None
+        return p
+
+    @plot.setter
+    def plot(self, p): self._plot = p
+
+
+def _margin(margin):
+    if type(margin) in (int, float): margin = 4 * (margin,)
+    return margin
+
+def _outer(lrbt, margin=0):
+    margin = _margin(margin)
+    return [lrbt[i] + (1 if i % 2 else -1) * margin[i] for i in range(4)]
+
+def _autoSize(lrbt, width, margin=0):
+    tmp = _outer(lrbt, margin)
+    return width, round(width * (tmp[3] - tmp[2]) / (tmp[1] - tmp[0]))
+
+def _str2gr(text, x, y, **kwargs):
+    xy = dict(x=x, y=y)
+    return Text(text.format(**xy)).config(**kwargs)
+
+
+class CoordinateSystem:
+
+    def coords(self, lrbt=None, size=None, margin=0):
+        first = not hasattr(self, "_lrbt")
+        if first:
+            if size is None: size = self.size
+            elif type(size) is int: size = _autoSize(lrbt, size, margin)
+            lrbt = _outer(lrbt, margin) if margin else calc_lrbt(lrbt, *size)
+        else:
+            lrbt = self._lrbt
+            size = self.size
+
+        self.pix = px = coordTr(lrbt, size)
+        self.unpix = ux = coordTr(lrbt, size, True)
         p0 = px((0, 0))
         p1 = px((1, 1))
         p0, p1 = p1[0] - p0[0], p1[1] - p0[1]
         self._units = p0, p1, sqrt(abs(p0 * p1)), p0 * p1 > 0
+
+        if first:
+            x0, y1 = ux((0, 0))
+            x1, y0 = ux(size)
+            self._lrbt = x0, x1, y0, y1
+        elif isinstance(self, Canvas):
+            for gr in self.instOf(Renderable): gr.stale = True
+
+    __init__ = coords
+
+    def transform(self, pts, **kwargs):
+        tr = self.unpix if kwargs.get("invert") else self.pix
+        for pt in pts: yield(tr(pt))
 
     @property
     def left(self): return self._lrbt[0]
@@ -58,6 +107,31 @@ class PCanvas(Canvas):
     def top(self): return self._lrbt[3]
 
     @property
+    def bottomleft(self):
+        c = self._lrbt
+        return c[0], c[2]
+
+    @property
+    def bottomright(self):
+        c = self._lrbt
+        return c[1], c[2]
+
+    @property
+    def topright(self):
+        c = self._lrbt
+        return c[1], c[3]
+
+    @property
+    def topleft(self):
+        c = self._lrbt
+        return c[0], c[3]
+
+    @property
+    def middle(self):
+        c = self._lrbt
+        return (c[0] + c[1]) / 2, (c[2] + c[3]) / 2
+
+    @property
     def clockwise(self): return self._units[3]
 
     @property
@@ -66,22 +140,11 @@ class PCanvas(Canvas):
     @property
     def unit(self): return self._units[2]
 
-    @property
-    def size(self): return self._size
-
-    @size.setter
-    def size(self, size): self.resize(size)
-
-    def resize(self, size):
-        super().resize(size)
-        self._makeTr()
-
     def flatten(self):
         self.config(bg=self.snapshot())
-        self.purge()
-        return self
+        return self.purge()
 
-    def grid(self, lrbt, interval, **kwargs):
+    def gridlines(self, lrbt, interval, **kwargs):
         style = {"weight":1, "stroke":"lightgrey"}
         style.update(kwargs)
         dx, dy = interval
@@ -106,26 +169,66 @@ class PCanvas(Canvas):
         if y: self += Line(px((0, y[0])), px((0, y[1]))).config(**style)
         return self
 
-    def points(self, points, markers, shift=(0, 0), **kwargs):
+    def graph(self, points, markers, shift=(0, 0), **kwargs):
         i = 0
         dx, dy = shift
         px = self.pix
         for x, y in points:
-            if type(markers) is str:
-                gr = Text(markers.format(x, y)).config(**kwargs)
+            t = type(markers)
+            if t in (int, float):
+                dx = markers / 2
+                pts = [(x-dx, 0), (x-dx, y), (x+dx, y), (x+dx, 0)]
+                self += Polygon(self.transform(pts)).config(**kwargs)
             else:
-                try: gr = markers[i]
-                except: gr = Image(markers)
-            self += gr.config(pos=px((x + dx, y + dy)))
+                if t is str: gr = _str2gr(markers, x, y, **kwargs)
+                else:
+                    try:
+                        gr = markers[i]
+                        if type(gr) is str: gr = _str2gr(gr, x, y, **kwargs)
+                    except: gr = Image(markers)
+                self += gr.config(pos=px((x + dx, y + dy)))
             i += 1
         return self
 
+# Labeling axes...
+# plot.mix(range(2, 11, 2), -0.5, "{x:d}", **font)
+# plot.mix(-0.1, range(-4, 5, 2), "{y:d}", anchor=RIGHT, **font)
 
-# Labelling the axis...
-#     self.points([(x, 0) for x in range(2, 10, 2)], "{0:d}", (0, -0.4))
+    def mix(self, x, y, markers, shift=(0, 0), **kwargs):
+        return self.graph(mix(x, y), markers, shift, **kwargs)
+
+    def zip(self, x, y, markers, shift=(0, 0), **kwargs):
+        return self.graph(zip(x, y), markers, shift, **kwargs)
 
 
-class Locus(oldPlot.Locus):
+class PCanvas(Canvas, CoordinateSystem):
+
+    def __init__(self, size, lrbt, margin=0, grid=None, bg=None):
+        if type(size) is int: size = _autoSize(lrbt, size, margin)
+        super().__init__(size, bg)
+        self.coords(lrbt, margin=margin)
+        if grid:
+            if type(grid) in (int, float): grid = 2 * [grid]
+            self.gridlines(lrbt, grid).axis(x=lrbt[:2], y=lrbt[2:])
+
+    def resize(self, size):
+        super().resize(size)
+        self.coords()
+
+
+class PSketch(Sketch, CoordinateSystem):
+
+    def __init__(self, size, lrbt, margin=0):
+        if type(size) is int: size = _autoSize(lrbt, size, margin)
+        super().__init__(size)
+        self.coords(lrbt, size, margin)
+
+    def resize(self, size, mode=None):
+        super().resize(size, mode)
+        self.coords(size=size)
+
+
+class Locus(oldPlot.Locus, _Plotable):
 
     def __init__(self, data, param, **kwargs):
         self.data = data
@@ -136,7 +239,7 @@ class Locus(oldPlot.Locus):
     def draw(self, srf, snapshot=False):
         if snapshot: x0, y0 = 0, 0
         else: x0, y0 = self.canvas.rect.topleft
-        px = getattr(self.canvas, "pix", None)
+        px = getattr(self.plot, "pix", None)
         d = self.data
         pts = d if type(d) in (list, tuple) else locus(d, self.param, **self.vars)
         if px: pts = (px(p) for p in pts)
@@ -147,21 +250,8 @@ class Locus(oldPlot.Locus):
         else: return pygame.Rect(0,0,0,0)
 
 
-class VectorGrid(PCanvas):
-
-    def __init__(self, width, lrbt, grid=1, margin=1, bg=None):
-
-        # Create plot canvas
-        tmp = [lrbt[i] + (1 if i % 2 else -1) * margin for i in range(4)]
-        height = round(width * (tmp[3] - tmp[1]) / (tmp[2] - tmp[0]))
-        super().__init__((width, height), tmp, bg)
-        
-        # Draw grid and axes
-        if grid: self.grid(lrbt, (grid, grid)).axis(x=lrbt[:2], y=lrbt[2:])
-
-
-class Vector(Renderable):
-    plot = None
+class Vector(Renderable, _Plotable):
+    autoPositionOnResize = False
     tail = 0, 0
     stroke = "red"
     weight = 3
@@ -214,11 +304,11 @@ class Vector(Renderable):
     def proj(self, u):
         "Return projection onto a vector or direction"
         u = Vector(1, u.dir if isinstance(u, Vector) else u)
-        return (u * (self * u)).config(plot=self.plot)
+        return (u * (self * u)).config(_plot=self._plot)
 
     def __add__(self, other):
         xy = sigma(self.xy, other.xy)
-        return Vector(xy=xy).config(plot=self.plot)
+        return Vector(xy=xy).config(_plot=self._plot)
 
     def __sub__(self, other):
         return self + other * -1
@@ -229,10 +319,10 @@ class Vector(Renderable):
             x2, y2 = other.xy
             return x1 * x2 + y1 * y2
         else:
-            return Vector(other * self.mag, self.dir).config(plot=self.plot)
+            return Vector(other * self.mag, self.dir).config(_plot=self._plot)
 
     def __truediv__(self, x):
-        return Vector(self.mag / x, self.dir).config(plot=self.plot)
+        return Vector(self.mag / x, self.dir).config(_plot=self._plot)
 
     def shift(self, dx=0, dy=0):
         tx, ty = self.tail
