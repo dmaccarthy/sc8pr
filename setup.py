a3ea720d2411eb640b730d73a8b68d756ee38acc
D. MacCarthy <sc8pr.py@gmail.com>
2021-10-01 11:30:52 -0600

    Update setup.py
    Redrawing bug when using pygame 2.0!

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
 
     # Dependencies
     python_requires = ">=3.4, <4",
-    install_requires = ["pygame>=1.9.2"],
+    install_requires = ["pygame>=1.9.2,<2.0.0"],
     
     # URLs
     url = "https://dmaccarthy.github.io/sc8pr",
