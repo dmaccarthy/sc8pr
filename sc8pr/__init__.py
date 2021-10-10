a46f3b5b4d59e717d722dfc99f8ffc36c3042838
D. MacCarthy <sc8pr.py@gmail.com>
2021-10-10 07:04:57 -0600

    Release 2.2a3

--- sc8pr/__init__.py
+++ sc8pr/__init__.py
@@ -16,7 +16,7 @@
 # along with "sc8pr".  If not, see <http://www.gnu.org/licenses/>.
 
 
-version = 2, 2, "dev3"
+version = 2, 2, "a3"
 print("sc8pr {}.{}.{}: https://dmaccarthy.github.io/sc8pr".format(*version))
 
 import sys, struct, zlib, io
