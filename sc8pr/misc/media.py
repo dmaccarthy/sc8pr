185100b1aa2a7d08086ea7d708dac9f41494d0e5
D. MacCarthy <sc8pr.py@gmail.com>
2021-10-13 15:47:21 -0600

    Update media.py
    Replace experimental ImageIO class by FFReader and FFWriter.

--- sc8pr/misc/media.py
+++ sc8pr/misc/media.py
@@ -15,9 +15,14 @@
 # You should have received a copy of the GNU General Public License
 # along with "sc8pr".  If not, see <http://www.gnu.org/licenses/>.
 
+"""
+Additional features for encoding and decoding media using additional packages
+    (imageio, imageio-ffmpeg, Pillow, numpy)
+"""
+
 
 import os, struct, pygame
-from sc8pr import PixelData
+from sc8pr import PixelData, Graphic, Image
 from sc8pr.misc.video import Video
 from sys import modules
 
@@ -48,40 +53,55 @@ class Grabber:
     def img(self): return self.pix.img
 
 
-class ImageIO:
-    "Use imageio and ffmpeg to decode and encode video"
+class _FF:
 
     @staticmethod
     def ffmpeg(ff): os.environ["IMAGEIO_FFMPEG_EXE"] = ff
 
-    @staticmethod
-    def decode(src, *args, save=None, purge=False, progress=None):
-        "Load a movie file as a list of Video clips"
-        if not args: args = (0,),
-        vid = [Video() for a in args]
-        with modules["imageio"].get_reader(src) as reader:
-            meta = reader.get_meta_data()
-            i, n = 0, meta.get("nframes")
-            for clip in vid:
-                clip.ffmpeg_meta = meta
-                clip.size = meta["size"]
-                clip.meta["frameRate"] = meta["fps"]
-            clip = 0
-            info = struct.pack("!3I", 0, *vid[0].size)
-            a = args[0]
-            try: # Extra frames/bad metadata in MKV?
-                for f in reader:
-                    if len(a) == 2 and a[1] == i:
-                        if save: vid[clip].save(save.format(clip))
-                        if purge: vid[clip].purge()
-                        clip += 1
-                        if clip >= len(args): break
-                        a = args[clip]
-                    if i >= a[0]: vid[clip] += bytes(f), info
-                    if progress: progress(i)
-                    i += 1
-            except: pass
-        return vid if args and len(args) > 1 else vid[0]
+    def __enter__(self): return self
+    def __exit__(self, *args): self._io.close()
+    close = __exit__
+
+
+class FFReader(_FF):
+    "Read images directly from a media file using imageio/FFmpeg"
+
+    def __init__(self, fn):
+        self._io = r = modules["imageio"].get_reader(fn)
+        self._iter = iter(self._io)
+        self.meta = r.get_meta_data()
+        self._info = struct.pack("!3I", 0, *self.meta["size"])
+
+    def __next__(self):
+        "Return the next frame as an uncompressed PixelData instance"
+        return PixelData((next(self._iter), self._info))
+
+    def read(self, n=None):
+        "Return a Video instance from the next n frames"
+        vid = Video()
+        while n or n is None:
+            try: vid += next(self._iter), self._info
+            except: n = 0
+            if n: n -= 1
+        return vid
+
+    def skip(self, n):
+        "Read and discard n frames"
+        while n:
+            try:
+                next(self._iter)
+                n -= 1
+            except: n = 0
+        return self
+
+    def __iter__(self):
+        "Iterate through all frames returning data as uncompressed PixelData"
+        for f in self._io:
+            yield PixelData((bytes(f), self._info))
+
+
+class FFWriter(_FF):
+    "Write graphics directly to media file using imageio/FFmpeg"
 
     @staticmethod
     def _createFrameData():
@@ -90,17 +110,26 @@ class ImageIO:
         if pil: return lambda img: np.array(img.pil)
         else: return lambda img: np.swapaxes(pygame.surfarray.array3d(img.srf), 0, 1)
 
-    @staticmethod
-    def encode(vid, save="movie.mp4", progress=None):
-        "Encode a movie from a Video instance or s8v file"
-        if type(vid) is str: vid = Video(vid)
-        fps = vid.meta.get("frameRate")
-        vid = vid.scaleFrames()
-        i, n = 1, len(vid)
-        with modules["imageio"].get_writer(save, fps=(fps if fps else 30)) as writer:
-            fd = ImageIO._createFrameData()
-            for img in vid.frames():
-                writer.append_data(fd(img))
-                if progress:
-                    progress(i, n)
-                    i += 1
+    def __init__(self, fn, fps=30):
+        self._fd = FFWriter._createFrameData()
+        self.size = None
+        self._io = modules["imageio"].get_writer(fn, fps=fps)
+
+    def write(self, img):
+        "Write one frame to the video file"
+        if not isinstance(img, Graphic):
+            img = Image(img)
+        if self.size is None: self.size = img.size
+        srf = img.image
+        if srf.get_size() != self.size:
+            srf = Image(srf).config(size=self.size).image
+        data = self._fd(PixelData(srf))
+#         elif img.size != self.size:
+#             img.config(size=self.size)
+#         data = self._fd(PixelData(img.snapshot()))
+        self._io.append_data(data)
+
+    def encode(self, vid):
+        "Encode a Video instance as a media file"
+        for frame in vid.scaleFrames().frames():
+            self._io.append_data(self._fd(frame))
