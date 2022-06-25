fa125ee340389b4d1b6c84836b4c77372615522a
D. MacCarthy <sc8pr.py@gmail.com>
2022-06-24 18:49:20 -0600

    Update __init__.py

--- sc8pr/plot/__init__.py
+++ sc8pr/plot/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2015-2021 D.G. MacCarthy <https://dmaccarthy.github.io/sc8pr>
+# Copyright 2015-2022 D.G. MacCarthy <https://dmaccarthy.github.io/sc8pr>
 #
 # This file is part of "sc8pr".
 #
