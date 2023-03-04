273585e34c8453952bd840958eda3253c61e31c4
D. MacCarthy <sc8pr.py@gmail.com>
2023-03-04 14:36:23 -0700

    Update video.py

--- sc8pr/misc/video.py
+++ sc8pr/misc/video.py
@@ -15,58 +15,111 @@
 # You should have received a copy of the GNU General Public License
 # along with "sc8pr".  If not, see <http://www.gnu.org/licenses/>.
 
-from sc8pr import PixelData, Image, BaseSprite
+from sc8pr import Image, BaseSprite, logError
+from sc8pr.util import surface, scale
 from sc8pr.sprite import CostumeImage, Sprite
 from zipfile import ZipFile, ZIP_DEFLATED
 from json import dumps, loads
 
+RGB = 24
+RGBA = 32
+_open_list = []
 
-class VidZip:
-    "A class for storing video frames in a ZIP archive"
 
-    capture_RGB = True
+class Video(ZipFile, CostumeImage):
+    "Sprites associated with a ZIP archive video"
+
+    @staticmethod
+    def closeAll():
+        for v in list(_open_list): v.close()
+
+    def __init__(self, zfile, **kwargs):
+        self._open(zfile, **kwargs)
+        try:
+            self._size = tuple(self.meta["size"])
+            self._info = self._size, "RGBA" if self.meta["bits"] == 32 else "RGB"
+            self._costume = self[0]
+        except:
+            self._costume = self._size = None
+
+    def __len__(self):
+        s = self._seq
+        return len(s) if s else self._nframes
+
+    @property
+    def costumeList(self): return self
+
+    @property
+    def fps(self): return self._meta.get("fps", 30)
+
+    @property
+    def costumeNumber(self): return self._costumeNumber
+ 
+    @costumeNumber.setter
+    def costumeNumber(self, n):
+        s = self._seq
+        self._costumeNumber = n = n % len(self)
+        self._costume = self[s[n] if s else n].rgba
+
+    def costume(self):
+        "Return an Image instance of the current costume"
+        return self._costume.config(size=self._size, angle=self.angle)
+
+    def clip(self, zfile, mode="x", start=0, frames=None):
+        m = self.meta
+        with Video(zfile, mode=mode) as zo:
+            clip = self[start:start+frames] if frames else self[start:]
+            for f in clip: zo.write(f)
+            zo.meta.update(m)
+            try: del zo.meta["duration"]
+            except: pass
+        return self
+
+
+# ZipFile reading and writing...
+
+    write_alpha = read_alpha = None
+
+    def _open(self, zfile, mode="r", **kwargs):
+        "Open the ZipFile and extract metadata"
+        attr = {"compression": ZIP_DEFLATED} if mode in ("w", "x") else {}
+        attr.update(kwargs)
+        super().__init__(zfile, mode, **attr)
+        _open_list.append(self)
+        try: meta = loads(self.read("meta.json"))
+        except: meta = {"nframes": 0}
+        self._meta = meta
+        self._nframes = meta["nframes"]
+        self._readN = self._readImg = self._append = None
+
+    def close(self, *args):
+        try: # May fail if file is read-only
+            if self._nframes != self._meta["nframes"]:
+                self._meta["nframes"] = self._nframes
+                self.writestr("meta.json", dumps(self._meta))
+        except: pass
+        super().close()
+        if self in _open_list: _open_list.remove(self)
+
+    __exit__ = close
 
     @property
     def meta(self): return self._meta
 
-    def __enter__(self): return self
-    def __len__(self): return self._nframes
-
-    def __exit__(self, *args):
-        try: # May fail if file is read-only
-            if self._nframes != self._meta["nframes"]:
-                self._meta["nframes"] = self._nframes
-                self._zf.writestr("meta.json", dumps(self._meta))
-        except: pass
-        self._zf.close()
-
-    close = __exit__
-
-    def __init__(self, zfile, mode="r", **kwargs):
-        attr = {"compression": ZIP_DEFLATED} if mode in ("w", "x") else {}
-        attr.update(kwargs)
-        self._zf = ZipFile(zfile, mode, **attr)
-        try: meta = loads(self._zf.read("meta.json"))
-        except: meta = {"nframes": 0}
-        self._meta = meta
-        self._nframes = meta["nframes"]
-        self._readN = self._readPix = self._append = None
-
     def _actual(self, n):
-        "Get the actual zipfile key for the requested frame"
+        "Get the actual ZipFile key for the requested frame"
         if n < 0: n += self._nframes
         if n < 0 or n >= self._nframes: return None
         data = None
-        zf = self._zf
         while n >= 0 and data is None:
             try:
-                zf.getinfo(str(n))
+                self.getinfo(str(n))
                 data = n
             except: n -= 1
         return data
 
     def _get_slice(self, start, stop, step=1):
-        "Generate PixelData instances for requested slice"
+        "Generate Image instances for requested slice"
         n = self._nframes
         if start is None: start = 0
         elif start < 0: start += n
@@ -75,74 +128,35 @@ class VidZip:
         for i in range(start, stop, step): yield self[i]
 
     def __getitem__(self, i):
-        "Return a PixelData or generator for the requested index or slice"
+        "Return an Image or generator for the requested index or slice"
         if type(i) is slice: return self._get_slice(i.start, i.stop, i.step if i.step else 1)
         i = self._actual(i)
         if i is None: raise IndexError("out of range")
         if i != self._readN:
             self._readN = 1
-            self._readPix = PixelData(self._zf.read(str(i)))
-        return self._readPix
+            img = Image.fromBytes((self.read(str(i)), self._info)).convert(self.read_alpha)
+            self._readImg = img
+        return self._readImg
 
     def write(self, *args, repeat=1):
-        "Write images to the zipfile"
+        "Write images to the ZipFile"
+        alpha = self.write_alpha
         for img in args:
-            if not isinstance(img, PixelData):
-                img = PixelData(img)
-            if self.capture_RGB and img.mode == "RGBA":
-                img = PixelData(img.srf.convert(24))
-            img = bytes(img)
-            if self._append is None or img != self._append: 
-                self._append = img
-                self._zf.writestr(str(self._nframes), img)
+            img = surface(img)
+            if self._nframes == 0:
+                self.meta["size"] = img.get_size()
+                self.meta["bits"] = img.get_bitsize() if alpha is None else 32 if alpha else 24
+            size = self.meta["size"]
+            if img.get_size() != size:
+                img = scale(img, size)
+            data = Image(img).convert(alpha).bytesTuple(False)
+            if self._append is None or data != self._append: 
+                self._append = data
+                self.writestr(str(self._nframes), data)
             self._nframes += repeat
         return self
 
-    __iadd__ = write #capture          
-
-    def clip(self, zfile, mode="x", start=0, frames=None):
-        m = self.meta
-        with VidZip(zfile, mode) as zo:
-            clip = self[start:start+frames] if frames else self[start:]
-            for f in clip: zo += f
-            zo.meta.update(m)
-            try: del zo.meta["duration"]
-            except: pass
-        return self
-
-
-class Video(CostumeImage):
-    "Sprites associated with a ZIP archive video"
-
-    def __init__(self, zfile, alpha=False, **kwargs):
-        self._vid = VidZip(zfile, **kwargs)  # Needs to be CLOSED!!
-        self._costume = self._vid[0].rgba if alpha else self._vid[0].img
-        self._size = self._costume.size
-
-    def __len__(self):
-        s = self._seq
-        return len(s if s else self._vid)
-
-    def close(self): self._vid._zf.close()
-
-    @property
-    def costumeList(self): return self._vid
-
-    @property
-    def fps(self): return self._vid._meta.get("fps", 30)
-
-    @property
-    def costumeNumber(self): return self._costumeNumber
- 
-    @costumeNumber.setter
-    def costumeNumber(self, n):
-        s = self._seq
-        self._costumeNumber = n = n % len(s if s else self._vid)
-        self._costume = self._vid[s[n] if s else n].rgba
-
-    def costume(self):
-        "Return an Image instance of the current costume"
-        return self._costume.config(size=self._size, angle=self.angle)
+    __iadd__ = write
 
 
 class VideoSprite(Video, BaseSprite):
