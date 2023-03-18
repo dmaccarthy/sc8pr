2b31f7516d06a51a86c4a7a68fdd3279c04b4ebc
D. MacCarthy <sc8pr.py@gmail.com>
2023-03-18 07:00:19 -0600

    Spiral effect

--- sc8pr/effect/stamp.py
+++ sc8pr/effect/stamp.py
@@ -1,7 +1,23 @@
+# Copyright 2015-2023 D.G. MacCarthy <http://dmaccarthy.github.io>
+#
+# This file is part of "sc8pr".
+#
+# "sc8pr" is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# "sc8pr" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with "sc8pr".  If not, see <http://www.gnu.org/licenses/>.
+
 import pygame
 from pygame.draw import circle, polygon
 from math import hypot, ceil, pi, cos, sin
-from sc8pr import Canvas
 from sc8pr.util import surface, rgba
 from sc8pr.effect import Effect
 from sc8pr.shape import Polygon
@@ -68,6 +84,9 @@ class Spiral(Stamp):
             yield r*cos(a), r*sin(a)
 
     def get_stamp(self, t, size):
-        cv = Canvas(size).attachCS([-0.7, 0.7, -0.7, 0.7])
-        cv += Polygon(self.points(t)).config(fill=self._fill, weight=0)
-        return cv.snapshot()
+        w, h = size
+        srf = pygame.Surface(size, pygame.SRCALPHA)
+        poly = Polygon(self.points(t), (0,0))
+        poly.transform(scale=(w/1.4, h/1.4)).config(pos=(w/2, h/2))
+        polygon(srf, self._fill, poly.vertices)
+        return srf
